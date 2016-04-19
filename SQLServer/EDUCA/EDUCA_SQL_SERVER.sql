5bf4b2d424cd5666f4402232a9c50447ba0fdcf7
Eric Gustavo Coronel Castillo <gcoronelc@gmail.com>
2016-04-19 05:17:19 -0500

    Bases de datos de SQL Server.

---
+++ SQLServer/EDUCA/EDUCA_SQL_SERVER.sql
@@ -0,0 +1,376 @@
+/**
+ *
+ * DBMS           :  SQL Server
+ * Base de Datos  :  EDUCA
+ * Descripción    :  Base de Datos de control académico sencillo
+ * Script         :  Crea la Base de Datos
+ * Creao por      :  Ing. Eric Gustavo Coronel Castillo
+ * Email          :  gcoronelc@gmail.com
+ * Web site       :  http://gcoronelc.blogspot.com
+ * Fecha          :  15-Enero-2009
+ * 
+**/
+
+
+-- ======================================================
+-- CREA LA BASE DE DATOS
+-- ======================================================
+
+USE master;
+GO
+
+IF( NOT EXISTS ( SELECT 1 FROM sys.sysdatabases WHERE name='EDUCA' ) )
+BEGIN
+	CREATE DATABASE EDUCA;
+END;
+GO
+
+USE EDUCA;
+GO
+
+-- ======================================================
+-- ELIMINA TABLA
+-- ======================================================
+
+IF( EXISTS ( SELECT 1 FROM sys.sysobjects 
+	WHERE name='PAGO' and xtype = 'u') )
+BEGIN
+	DROP TABLE dbo.PAGO;
+END;
+GO
+
+IF( EXISTS ( SELECT 1 FROM sys.sysobjects 
+	WHERE name='MATRICULA' and xtype = 'u') )
+BEGIN
+	DROP TABLE dbo.MATRICULA;
+END;
+GO
+
+IF( EXISTS ( SELECT 1 FROM sys.sysobjects 
+	WHERE name='CURSO' and xtype = 'u') )
+BEGIN
+	DROP TABLE dbo.CURSO;
+END;
+GO
+
+IF( EXISTS ( SELECT 1 FROM sys.sysobjects 
+	WHERE name='ALUMNO' and xtype = 'u') )
+BEGIN
+	DROP TABLE dbo.ALUMNO;
+END;
+GO
+
+-- ======================================================
+-- TABLA ALUMNO
+-- ======================================================
+
+CREATE TABLE dbo.ALUMNO
+( 
+	alu_id               INT  NOT NULL ,
+	alu_nombre           varchar(100)  NOT NULL ,
+	alu_direccion        varchar(100)  NOT NULL ,
+	alu_telefono         varchar(20)  NULL ,
+	alu_email            varchar(50)  NULL 	
+);
+GO
+
+-- ======================================================
+-- TABLA CURSO
+-- ======================================================
+
+CREATE TABLE dbo.CURSO
+( 
+	cur_id               INT IDENTITY ( 1,1 ) NOT NULL ,
+	cur_nombre           varchar(100)  NOT NULL ,
+	cur_vacantes         int  NOT NULL ,
+	cur_matriculados     int  NOT NULL ,
+	cur_profesor         varchar(100)  NULL ,
+	cur_precio           money  NOT NULL 
+);
+GO
+
+
+-- ======================================================
+-- TABLA MATRICULA
+-- ======================================================
+
+
+CREATE TABLE dbo.MATRICULA
+( 
+	cur_id               INT  NOT NULL ,
+	alu_id               INT  NOT NULL ,
+	mat_fecha            datetime  NOT NULL ,
+	mat_precio           money  NOT NULL ,
+	mat_cuotas           int  NOT NULL ,
+	mat_nota             int  NULL 
+);
+GO
+
+
+-- ======================================================
+-- TABLA PAGO
+-- ======================================================
+
+CREATE TABLE dbo.PAGO
+( 
+	cur_id               INT  NOT NULL ,
+	alu_id               INT  NOT NULL ,
+	pag_cuota            int  NOT NULL ,
+	pag_fecha            datetime  NOT NULL ,
+	pag_importe          money  NOT NULL 
+);
+GO
+
+-- ======================================================
+-- RESTRICCIONES DE LA TABLA ALUMNO
+-- ======================================================
+
+ALTER TABLE dbo.ALUMNO
+	ADD CONSTRAINT PK_ALUMNO 
+	PRIMARY KEY CLUSTERED (alu_id ASC);
+go
+
+ALTER TABLE dbo.ALUMNO
+	ADD CONSTRAINT U_ALUMNO_EMAIL 
+	UNIQUE (alu_email  ASC);
+go
+
+ALTER TABLE dbo.ALUMNO
+	ADD CONSTRAINT U_ALUMNO_NOMBRE 
+	UNIQUE (alu_nombre  ASC);
+go
+
+INSERT INTO ALUMNO (alu_id, alu_nombre, alu_direccion, alu_telefono, alu_email )
+VALUES
+( 1,'YESENIA VIRHUEZ','LOS OLIVOS','986412345','yesenia@hotmail.com'),
+( 2,'OSCAR ALVARADO FERNANDEZ','MIRAFLORES',NULL,'oscar@gmail.com'),
+( 3,'GLADYS REYES CORTIJO','SAN BORJA','875643562','gladys@hotmail.com'),
+( 4,'SARA RIEGA FRIAS','SAN ISIDRO',NULL,'sara@yahoo.com'),
+( 5,'JHON VELASQUEZ DEL CASTILLO','LOS OLIVOS','78645345','jhon@movistar.com'),
+( 6,'RODRIGUEZ ROJAS, RENZO ROBERT','SURCO','673465235','rrodrigiez@gmail.com'),
+( 7,'CALERO MORALES, EMELYN DALILA','LA MOLINA','896754652','ecalero@peru.com'),
+( 8,'KAREN FUENTES','San Isidro','555-5555','KAFUENTES@HOTMAIL.COM'),
+( 9,'Yamina Ruiz','San Isidro','965-4521','yami_ruiz@gmail.com'),
+(10,'MARIA EULALIA VELASQUEZ TORVISCO','SURCO','6573456','mvelasques@gmail.com'),
+(11,'FIORELLA LIZET VITELLA REYES','SAN BORJA','5468790','fvitela@outlook.com');
+GO
+
+-- ======================================================
+-- RESTRICCIONES DE LA TABLA CURSO
+-- ======================================================
+
+-- CLAVE PRIMARIA
+
+ALTER TABLE dbo.CURSO
+	ADD CONSTRAINT pk_curso 
+	PRIMARY KEY CLUSTERED (cur_id ASC);
+go
+
+
+-- El nombre del curso es único
+
+ALTER TABLE dbo.CURSO
+	ADD CONSTRAINT u_curso_nombre 
+	UNIQUE (cur_nombre  ASC);
+go
+
+-- Vacantes mayor que cero
+
+ALTER TABLE dbo.CURSO
+	ADD CONSTRAINT  chk_curso_vacantes
+		CHECK  ( cur_vacantes > 0 ); 
+go
+
+-- Matriculados mayor o igual que cero, y menor o igual que las vacantes
+
+ALTER TABLE dbo.CURSO
+	ADD CONSTRAINT  chk_curso_matriculados
+		CHECK  ( cur_matriculados >= 0 AND cur_matriculados <= cur_vacantes ) ;
+go
+
+-- Precio mayor que cero
+ALTER TABLE dbo.CURSO
+	ADD CONSTRAINT  chk_curso_precio
+		CHECK  ( cur_precio > 0 );
+go
+
+
+-- Matriculados por defecto es CERO
+
+ALTER TABLE dbo.CURSO
+	ADD CONSTRAINT DF_CURSO_MATRICULADOS
+		 DEFAULT  0 FOR cur_matriculados
+go
+
+
+-- Insertar Datos
+
+SET IDENTITY_INSERT dbo.Curso ON;
+GO
+
+INSERT INTO CURSO(CUR_ID,CUR_NOMBRE,CUR_VACANTES,CUR_PRECIO,CUR_PROFESOR)
+VALUES(1,'SQL Server Implementación',24,1000.0,'Gustavo coronel');
+
+INSERT INTO CURSO(cur_id,cur_nombre,cur_vacantes,cur_precio,cur_profesor)
+VALUES(2,'SQL Server Administración',24,1000.0,' ');
+
+INSERT INTO CURSO(cur_id,cur_nombre,cur_vacantes,cur_precio,cur_profesor)
+VALUES(3,'Inteligencia de Negocios',24,1500.0,'Sergio Matsukawa');
+
+INSERT INTO CURSO(cur_id,cur_nombre,cur_vacantes,cur_precio,cur_profesor)
+VALUES(4,'Programación Transact-SQL',24,1200.0,NULL);
+
+INSERT INTO CURSO(cur_id,cur_nombre,cur_vacantes,cur_precio,cur_profesor)
+VALUES(5,'Java Fundamentos',24,1600.0,'Gustavo Coronel');
+
+INSERT INTO CURSO(cur_id,cur_nombre,cur_vacantes,cur_precio,cur_profesor)
+VALUES(6,'Java Cliente-Servidor',24,1600.0,'Gustavo Coronel');
+
+INSERT INTO CURSO(CUR_ID,CUR_NOMBRE,CUR_VACANTES,CUR_PRECIO,CUR_PROFESOR)
+VALUES(7,'GESTION DE PROYECTOS',24,2200.0,'RICARDO MARCELO');
+GO
+
+SET IDENTITY_INSERT dbo.Curso OFF;
+GO
+
+
+-- ======================================================
+-- RESTRICCIONES DE LA TABLA MATRICULA
+-- ======================================================
+
+ALTER TABLE dbo.MATRICULA
+	ADD CONSTRAINT PK_MATRICULA 
+	PRIMARY KEY CLUSTERED (cur_id ASC,alu_id ASC);
+go
+
+ALTER TABLE dbo.MATRICULA
+	ADD CONSTRAINT FK_MATRICULA_CURSO 
+	FOREIGN KEY (cur_id) 
+	REFERENCES dbo.CURSO(cur_id)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION;
+GO
+
+ALTER TABLE dbo.MATRICULA
+	ADD CONSTRAINT FK_MATRICULA_ALUMNO 
+	FOREIGN KEY (alu_id) 
+	REFERENCES dbo.ALUMNO(alu_id)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION;
+go
+
+
+ALTER TABLE dbo.MATRICULA
+	ADD CONSTRAINT  CHK_MATRICULA_PRECIO
+		CHECK  ( MAT_PRECIO > 0.0 );
+go
+
+ALTER TABLE dbo.MATRICULA
+	ADD CONSTRAINT  CHK_MATRICULA_CUOTAS
+		CHECK  ( MAT_CUOTAS > 0 );
+go
+
+ALTER TABLE dbo.MATRICULA
+	ADD CONSTRAINT  CHK_MATRICULA_NOTA
+		CHECK  ( (MAT_NOTA = NULL) OR (MAT_NOTA BETWEEN 0 AND 20) );
+go
+
+
+
+
+SET DATEFORMAT DMY
+GO
+
+DECLARE @ANIO VARCHAR(10);
+SET @ANIO =  cast(year(getdate()) as varchar);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(1, 5,'15-04-' + @ANIO +' 10:30',800.0,1,15);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(1, 3,'16-04-' + @ANIO +' 11:45',1000.0,2,18);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(1, 4,'18-04-' +@ANIO +' 08:33',1200.0,3,12);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(2, 1,'15-04-' + @ANIO +' 12:33',800.0,1,16);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(2, 2,'01-05-' + @ANIO +' 15:34',1000.0,2,10);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(2, 3,'03-05-' + @ANIO +' 16:55',1300.0,3,14);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(2, 4,'04-05-' + @ANIO +' 17:00',400.0,1,18);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(2, 5,'06-05-' + @ANIO +' 13:12',750.0,1,17);
+
+INSERT INTO dbo.MATRICULA ( cur_id, alu_id, mat_fecha, mat_precio, mat_cuotas, mat_nota ) 
+VALUES(3, 7,'02-06-' + @ANIO +' 13:12',950.0,2,15);
+
+GO
+
+
+-- ======================================================
+-- Actualizar la columna matriculados en la tabla curso
+-- ======================================================
+
+UPDATE dbo.CURSO
+SET cur_matriculados = (
+	SELECT COUNT(*) FROM dbo.MATRICULA
+	WHERE dbo.MATRICULA.cur_id = dbo.CURSO.cur_id );
+GO
+
+-- ======================================================
+-- RESTRICCIONES EN LA TABLA PAGO
+-- ======================================================
+
+-- Clave Primaria
+
+ALTER TABLE dbo.PAGO
+	ADD CONSTRAINT PK_PAGO 
+	PRIMARY KEY CLUSTERED (cur_id ASC,alu_id ASC,pag_cuota ASC);
+go
+
+-- Clave Foránea
+
+ALTER TABLE dbo.PAGO
+	ADD CONSTRAINT FK_PAGO_MATRICULA 
+	FOREIGN KEY (cur_id,alu_id) 
+	REFERENCES dbo.MATRICULA(cur_id,alu_id)
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+go
+
+-- Cargar Datos
+
+set dateformat dmy
+go
+
+declare @anio varchar(10)
+set @anio = cast(year(getdate()) as varchar)
+insert into dbo.PAGO values(1,3,1,'16-04-' + @anio,500)
+insert into dbo.PAGO values(1,3,2,'16-05-' + @anio,500)
+insert into dbo.PAGO values(1,4,1,'18-04-' + @anio,400)
+insert into dbo.PAGO values(1,4,2,'18-05-' + @anio,400)
+insert into dbo.PAGO values(2,1,1,'15-04-' + @anio,800)
+insert into dbo.PAGO values(2,2,1,'01-05-' + @anio,500)
+insert into dbo.PAGO values(2,3,1,'03-05-' + @anio,430)
+insert into dbo.PAGO values(2,3,2,'03-06-' + @anio,430)
+insert into dbo.PAGO values(2,4,1,'04-05-' + @anio,400)
+insert into dbo.PAGO values(2,5,1,'06-05-' + @anio,750)
+go
+
+
+-- ======================================================
+-- FIN
+-- ======================================================
+
+select * from dbo.ALUMNO
+select * from dbo.CURSO
+select * from dbo.MATRICULA
+select * from dbo.PAGO
+go
---
+++ SQLServer/RRHH/RRHH.sql
@@ -0,0 +1,282 @@
+/*
+ *
+ * Empresa        :  PeruDev
+ * DBMS           :  SQL Server 2008
+ * Base de Datos  :  RH
+ * Descripción    :  Base de Datos de Recursos Humanos
+ * Script         :  Crea la Base de Datos
+ * Responsable    :  Ing. Eric Gustavo Coronel Castillo
+ * Email          :  gcoronelc@gmail.com
+ * Blog           :  gcoronelc.blogspot.com
+ * Fecha          :  10-Mayo-2011
+ *
+*/
+
+-- ==========================================================
+-- Creación de la Base de Datos
+-- ==========================================================
+
+USE master;
+GO
+
+IF NOT EXISTS ( SELECT name FROM sys.sysdatabases WHERE name = 'RH' )
+BEGIN
+	CREATE DATABASE RH;
+END;
+GO
+
+-- ==========================================================
+-- Seleccionar la Base de Datos
+-- ==========================================================
+
+USE RH
+GO
+
+-- ==========================================================
+-- Eliminar las tablas en caso existan
+-- ==========================================================
+
+IF EXISTS ( SELECT 1 FROM sys.sysobjects WHERE name = 'empleado' AND xtype='U' )
+BEGIN
+	DROP TABLE dbo.empleado;
+END;
+GO
+
+IF EXISTS ( SELECT 1 FROM sys.sysobjects WHERE name = 'departamento' AND xtype='U' )
+BEGIN
+	DROP TABLE dbo.departamento;
+END;
+GO
+
+IF EXISTS ( SELECT 1 FROM sys.sysobjects WHERE name = 'ubicacion' AND xtype='U' )
+BEGIN
+	DROP TABLE dbo.ubicacion;
+END;
+GO
+
+IF EXISTS ( SELECT 1 FROM sys.sysobjects WHERE name = 'cargo' AND xtype='U' )
+BEGIN
+	DROP TABLE dbo.cargo;
+END;
+GO
+
+IF EXISTS ( SELECT 1 FROM sys.sysobjects WHERE name = 'control' AND xtype='U' )
+BEGIN
+	DROP TABLE dbo.control;
+END;
+GO
+
+-- ==========================================================
+-- Crear la Tabla de Puestos de Trabajo: cargo
+-- ==========================================================
+
+CREATE TABLE dbo.cargo (
+    idcargo     CHAR(3) NOT NULL,
+    nombre      VARCHAR(50) NOT NULL,
+    sueldo_min  MONEY NOT NULL,
+    sueldo_max  MONEY NOT NULL,
+    CONSTRAINT pk_cargo PRIMARY KEY ( idcargo )
+);
+go
+
+-- ==========================================================
+-- Crear la Tabla de Oficinas: ubicacion
+-- ==========================================================
+
+CREATE TABLE dbo.ubicacion (
+    idubicacion   CHAR(3) NOT NULL,
+    ciudad        VARCHAR(50) NOT NULL,
+    direccion     VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_ubicacion PRIMARY KEY ( idubicacion )
+);
+go
+
+-- ==========================================================
+-- Crear la Tabla de Departamentos: departamento
+-- ==========================================================
+
+CREATE TABLE dbo.departamento (
+    iddepartamento  INT NOT NULL,
+    nombre          VARCHAR(100) NOT NULL,
+    idubicacion     CHAR(3) NOT NULL,
+    CONSTRAINT pk_departamento 
+		PRIMARY KEY ( iddepartamento ),
+    CONSTRAINT fk_departamento_ubicacion 
+        FOREIGN KEY ( idubicacion ) 
+        REFERENCES dbo.ubicacion ( idubicacion )
+        ON DELETE NO ACTION 
+        ON UPDATE NO ACTION
+);
+go
+
+-- ==========================================================
+-- Crear la Tabla de Empleados: empleado
+-- ==========================================================
+
+CREATE TABLE dbo.empleado (
+    idempleado     CHAR(5)        NOT NULL,
+    apellido       VARCHAR(50)    NOT NULL,
+    nombre         VARCHAR(50)    NOT NULL,
+    fecingreso     smalldatetime  NOT NULL,
+    email          VARCHAR(50)    NULL,
+    telefono       VARCHAR(20)    NULL,
+    idcargo        CHAR(3)        NOT NULL,
+    iddepartamento INT            NOT NULL,
+    sueldo         MONEY          NOT NULL,
+    comision       MONEY     NULL,
+    jefe           CHAR(5)        NULL,
+    CONSTRAINT pk_empleado 
+        PRIMARY KEY ( idempleado ),
+    CONSTRAINT fk_empleado_cargo 
+        FOREIGN KEY ( idcargo ) 
+        REFERENCES dbo.cargo ( idcargo )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT fk_empleado_departamento 
+        FOREIGN KEY ( iddepartamento ) 
+        REFERENCES dbo.departamento ( iddepartamento )
+        ON DELETE NO ACTION 
+        ON UPDATE NO ACTION,
+    CONSTRAINT fk_empleado_empleado 
+        FOREIGN KEY ( jefe ) 
+        REFERENCES dbo.empleado ( idempleado )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+);
+GO
+
+-- ==========================================================
+-- Crear la Tabla de Parámetros: control
+-- ==========================================================
+
+CREATE TABLE dbo.control (
+    parametro   VARCHAR(20) NOT NULL,
+    valor       VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_control PRIMARY KEY ( parametro )
+);
+go
+
+
+-- ==========================================================
+-- Cargar datos a la tabla: cargo
+-- ==========================================================
+
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C01', 'Gerente General', 15000, 50000 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C02', 'Gerente', 10000, 15000 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C03', 'Empleado', 7000, 10000 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C04', 'Analista', 5000, 7000 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C05', 'Vendedor', 3000, 5000 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C06', 'Oficinista', 1500, 3000 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C07', 'Programador', 2500, 6000 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C08', 'Investigador', 6000, 8000 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C09', 'Digitador', 1000, 1500 );
+INSERT INTO cargo ( idcargo, nombre, sueldo_min, sueldo_max ) VALUES ( 'C10', 'Anfitriona', 1300, 1800 );
+GO
+
+-- ==========================================================
+-- Cargar datos a la tabla: ubicacion
+-- ==========================================================
+
+INSERT INTO ubicacion ( idubicacion, ciudad, direccion ) VALUES ( 'U01', 'Lima', 'Av. Benavides 534 - Miraflores' );
+INSERT INTO ubicacion ( idubicacion, ciudad, direccion ) VALUES ( 'U02', 'Trujillo', 'Calle Primavera 1235 - Cercado' );
+INSERT INTO ubicacion ( idubicacion, ciudad, direccion ) VALUES ( 'U03', 'Arequipa', 'Av. Central 2517 - Cercado' );
+INSERT INTO ubicacion ( idubicacion, ciudad, direccion ) VALUES ( 'U04', 'Lima', 'Av. La Marina 3456 - San Miguel' );
+GO
+
+-- ==========================================================
+-- Cargar datos a la tabla: departamento
+-- ==========================================================
+
+INSERT INTO departamento ( iddepartamento, nombre, idubicacion ) VALUES ( 100, 'Gerencia', 'U01' );
+INSERT INTO departamento ( iddepartamento, nombre, idubicacion ) VALUES ( 101, 'Contabilidad', 'U01' );
+INSERT INTO departamento ( iddepartamento, nombre, idubicacion ) VALUES ( 102, 'Investigacion', 'U03' );
+INSERT INTO departamento ( iddepartamento, nombre, idubicacion ) VALUES ( 103, 'Ventas', 'U01' );
+INSERT INTO departamento ( iddepartamento, nombre, idubicacion ) VALUES ( 104, 'Operaciones', 'U01' );
+INSERT INTO departamento ( iddepartamento, nombre, idubicacion ) VALUES ( 105, 'Sistemas', 'U04' );
+INSERT INTO departamento ( iddepartamento, nombre, idubicacion ) VALUES ( 106, 'Recursos Humanos', 'U04' );
+INSERT INTO departamento ( iddepartamento, nombre, idubicacion ) VALUES ( 107, 'Finanzas', 'U01' );
+GO
+
+-- ==========================================================
+-- Cargar datos a la tabla: empleado
+-- ==========================================================
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0001', 'Coronel', 'Gustavo', '20000401', 'gcoronelc@gmail.com', '996-664-457', 'C01', 100, 25000, NULL, NULL );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0002', 'Fernandez', 'Claudia', '20000501', 'cfernandez@perudev.com', '9345-8365', 'C03', 100, 8000, NULL, 'E0001' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0003', 'Matsukawa', 'Sergio', '20000401', 'smatsukawa@perudev.com', '9772-8369', 'C02', 102, 15000, NULL, 'E0001' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0004', 'Diaz', 'Mariela', '20000410', 'mdiaz@perudev.com', '8654-6734', 'C06', 102, 1800, NULL, 'E0003' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0005', 'Martinez', 'Roberto', '20000405', 'rmartinez@perudev.com', NULL, 'C08', 102, 9000, 500, 'E0003' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0006', 'Espinoza', 'Miguel', '20000406', 'mespinoza@perudev.com', NULL, 'C08', 102, 7500, 500, 'E0003' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0007', 'Ramos', 'Vanessa', '20020406', 'vramos@perudev.com', '9456-3456', 'C08', 102, 7000, 500, 'E0003' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0008', 'Flores', 'Julio', '20000401', 'jflores@perudev.com', NULL, 'C07', 102, 3500, 1000, 'E0003' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0009', 'Marcelo', 'Ricardo', '20000401', 'rmarcelo@perudev.com', '9936-2966', 'C02', 101, 15000, NULL, 'E0001' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0010', 'Barrios', 'Guisella', '20010115', 'gbarrios@perudev.com', '9023-4512', 'C03', 101, 8000, NULL, 'E0009' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0011', 'Cuellar', 'Lucy', '20030218', 'lcuellar@perudev.com', NULL, 'C06', 101, 2000, NULL, 'E0009' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0012', 'Valencia', 'Hugo', '20000501', 'hvalencia@perudev.com', '9732-5601', 'C02', 105, 18000, NULL, 'E0001' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0013', 'Veliz', 'Fortunato', '20000505', 'fveliz@perudev.com', '9826-7603', 'C04', 105, 6000, NULL, 'E0012' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0014', 'Aguero', 'Octavio', '20000515', 'oaguero@perudev.com', NULL, 'C07', 105, 3000, 300, 'E0012' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0015', 'Rosales', 'Cesar', '20030315', 'crosales@perudev.com', NULL, 'C07', 105, 2500, 300, 'E0012' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0016', 'Villarreal', 'Nora', '20000501', 'nvillarreal@perudev.com', '9371-3641', 'C02', 103, 15000, NULL, 'E0001' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0017', 'Romero', 'Alejandra', '20000503', 'aromero@perudev.com', '8345-9526', 'C03', 103, 7500, NULL, 'E0016' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0018', 'Valdiviezo', 'Jennifer', '20000612', 'jvaldiviezo@perudev.com', '9263-5172', 'C06', 103, 2000, NULL, 'E0016' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0019', 'Muchotrigo', 'Omar', '20000512', 'omuchotrigo@perudev.com', '9963-5542', 'C05', 103, 3500, 500, 'E0016' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0020', 'Baltazar', 'Victor', '20000518', 'vbaltazar@perudev.com', '9893-4433', 'C05', 103, 3000, 800, 'E0016' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0021', 'Castillo', 'Ronald', '20010518', 'rcastillo@perudev.com', '9234-3487', 'C05', 103, 3000, 800, 'E0016' );
+
+INSERT INTO empleado ( idempleado, apellido, nombre, fecingreso, email, telefono, idcargo, iddepartamento, sueldo, comision, jefe )
+VALUES ( 'E0022', 'Espilco', 'Luis', '20020417', 'lespilco@perudev.com', '9554-3456', 'C05', 103, 3000, 300, 'E0016' );
+
+GO
+
+-- ==========================================================
+-- Cargar datos a la tabla: control
+-- ==========================================================
+
+INSERT INTO control ( parametro, valor ) VALUES ( 'Cargo', '10' );
+INSERT INTO control ( parametro, valor ) VALUES ( 'Ubicacion', '4' );
+INSERT INTO control ( parametro, valor ) VALUES ( 'Departamento', '107' );
+INSERT INTO control ( parametro, valor ) VALUES ( 'Empleado', '22' );
+INSERT INTO control ( parametro, valor ) VALUES ( 'Empresa', 'PeruDev' );
+GO
+
