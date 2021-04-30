aaba9a1456be5cb54af69642f94a37ae4851f575
Gustavo Coronel <gcoronelc@gmail.com>
2021-04-29 20:47:25 -0500

    BookStore

--- EurekaBank/Oracle/procedimientos/usp_egcc_saldo_cuenta.sql
+++ EurekaBank/Oracle/procedimientos/usp_egcc_saldo_cuenta.sql
@@ -7,7 +7,7 @@
 
 -- Procedimiento
 
-create or replace procedure usp_egcc_saldo_cuenta
+create or replace procedure EUREKA.usp_egcc_saldo_cuenta
 ( p_cuenta varchar2, p_saldo out number )
 is
 begin
@@ -15,8 +15,13 @@ begin
 	select dec_cuensaldo into p_saldo
 	from cuenta
 	where chr_cuencodigo = p_cuenta;
+    
+exception
 
-end;
+    when no_data_found then
+        p_saldo := -100;
+
+end usp_egcc_saldo_cuenta;
 /
 
 -- Prueba
@@ -25,9 +30,14 @@ set serveroutput on
 
 declare
 	v_saldo number;
+    v_cuenta varchar(10) := '00100001';
 begin
-	usp_egcc_saldo_cuenta('00100001',v_saldo);
-	dbms_output.put_line('Saldo: ' || v_saldo);
+	usp_egcc_saldo_cuenta(v_cuenta,v_saldo);
+    if v_saldo = -100.0 then
+    	dbms_output.put_line('Saldo: ' || v_cuenta || ' no existe');
+    else 
+        dbms_output.put_line('Saldo: ' || v_saldo);
+    end if;
 end;
 /
 
