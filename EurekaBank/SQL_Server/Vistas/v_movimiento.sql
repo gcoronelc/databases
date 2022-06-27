b48d31610e2b0b8e497c4f80c07c91057cc21087
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-27 08:11:53 -0500

    EurekaBank

---
+++ EurekaBank/SQL_Server/Vistas/v_movimiento.sql
@@ -0,0 +1,64 @@
+USE EUREKABANK;
+GO
+
+DROP VIEW dbo.v_movimiento;
+GO
+
+create  view dbo.v_movimiento(
+sucucodigo, sucunombre, cliecodigo, cliepaterno,
+cliematerno, clienombre, cuencodigo, cuensaldo,
+cuenestado, movinumero, movifecha, moviimporte,
+cuenreferencia, tipocodigo, tiponombre, tipoaccion,
+monecodigo, monenombre)
+as
+select 
+  su.chr_sucucodigo  sucucodigo,
+  su.vch_sucunombre  sucunombre,
+  cl.chr_cliecodigo  cliecodigo,
+  cl.vch_cliepaterno cliepaterno,
+  cl.vch_cliematerno cliematerno,
+  cl.vch_clienombre  clienombre,
+  c.chr_cuencodigo  cuencodigo,
+  c.dec_cuensaldo   cuensaldo,
+  c.vch_cuenestado  cuenestado,
+  m.int_movinumero  movinumero,
+  m.dtt_movifecha   movifecha,
+  m.dec_moviimporte moviimporte,
+  m.chr_cuenreferencia cuenreferencia,
+  tm.chr_tipocodigo  tipocodigo,
+  tm.vch_tipodescripcion tiponombre,
+  tm.vch_tipoaccion  tipoaccion,
+  mo.chr_monecodigo  monecodigo,
+  mo.vch_monedescripcion monenombre
+from tipomovimiento tm
+join movimiento m on tm.chr_tipocodigo = m.chr_tipocodigo
+join cuenta c on m.chr_cuencodigo = c.chr_cuencodigo
+join moneda mo on c.chr_monecodigo = mo.chr_monecodigo
+join cliente cl on c.chr_cliecodigo = cl.chr_cliecodigo
+join sucursal su on c.chr_sucucodigo = su.chr_sucucodigo;
+GO
+
+
+
+select * from dbo.v_movimiento
+where cuencodigo='00100002';
+GO
+
+select 
+CUENCODIGO,
+MONENOMBRE,
+CUENSALDO,
+CUENESTADO,
+MOVINUMERO,
+MOVIFECHA,
+MOVIIMPORTE,
+TIPOCODIGO,
+TIPONOMBRE
+from v_movimiento
+GO
+
+
+
+
+
+
